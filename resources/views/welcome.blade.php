6c1f4e47e518f25f8cfd561cd99718617d883712
poymanov <n.poymanov@gmail.com>
2020-12-15 21:40:13 +0300

    Добавлена вспомогательная библиотека

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,6 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css', 'assets') }}">
-
-        <style>
-            body {
-                font-family: 'Nunito';
-            }
-        </style>
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
@@ -35,8 +29,8 @@
             @endif
             <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
                 <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl mr-7">
-                    <span class="block">Вы готовы начать планировать свою жизнь?</span>
-                    <span class="block text-indigo-600">Приступите к созданию задач на день прямо сейчас!</span>
+                    <span class="block">Вы готовы планировать свою жизнь?</span>
+                    <span class="block text-indigo-600">Начните это прямо сейчас!</span>
                 </h2>
                 <div class="mt-8 lex lg:mt-0 lg:flex-shrink-0">
                     <div class="inline-flex rounded-md shadow">
