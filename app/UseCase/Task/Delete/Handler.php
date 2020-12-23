a8bac51f22d7dc3587c66db6de9decf1300a533a
Nicolay Poymanov <n.poymanov@gmail.com>
2020-12-23 10:46:49 +0300

    Добавлено удаление шагов при удалении задачи

--- app/UseCase/Task/Delete/Handler.php
+++ app/UseCase/Task/Delete/Handler.php
@@ -6,6 +6,7 @@ namespace App\UseCase\Task\Delete;
 
 use App\Models\Task;
 use Exception;
+use Illuminate\Support\Facades\DB;
 
 class Handler
 {
@@ -17,8 +18,20 @@ class Handler
             throw new Exception(__('task.not_found'));
         }
 
+        DB::beginTransaction();
+
+        if ($task->steps()->exists()) {
+            if (!$task->steps()->delete()) {
+                DB::rollBack();
+                throw new Exception(__('step.failed_to_delete'));
+            }
+        }
+
         if (!$task->delete()) {
+            DB::rollBack();
             throw new Exception(__('task.delete_failed'));
         }
+
+        DB::commit();
     }
 }
--- tests/Feature/Task/DeleteTest.php
+++ tests/Feature/Task/DeleteTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Task;
 
+use App\Models\Step;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -64,6 +65,33 @@ class DeleteTest extends TestCase
 
         $this->assertDatabaseMissing('tasks', [
             'id' => $task->id,
+        ]);
+
+        $response->assertRedirect('/tasks');
+    }
+
+    /**
+     * Успешное удаление задачи вместе с шагами
+     */
+    public function test_delete_successful_with_steps()
+    {
+        $user = User::factory()->create();
+        $this->signIn($user);
+
+        $task = Task::factory()->create(['user_id' => $user->id]);
+        $stepFirst = Step::factory()->create(['task_id' => $task->id]);
+        $stepSecond = Step::factory()->create(['task_id' => $task->id]);
+
+        $response = $this->delete(self::BASE_URL . $task->id);
+
+        $response->assertSessionHas('alert.success');
+
+        $this->assertDatabaseMissing('steps', [
+            'id' => $stepFirst->id,
+        ]);
+
+        $this->assertDatabaseMissing('steps', [
+            'id' => $stepSecond->id,
         ]);
 
         $response->assertRedirect('/tasks');
